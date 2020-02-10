4070f12892082a041d597b005754de1aa6592b79
Damian <damian.smietana97@gmail.com>
2020-02-10 21:15:11 +0100

    Taking first steps

--- Main.py
+++ Main.py
@@ -2,5 +2,56 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((1600,900))
+screenWidth = 640
+screenHeight = 480
+
+win = pygame.display.set_mode((screenWidth,screenHeight))
 pygame.display.set_caption("Pygame game!")
+
+x = 50
+y = 430
+width = 40
+height = 60
+vel = 5
+
+isJump = False
+jumpCount = 10
+
+run = True
+while run:
+    pygame.time.delay(50)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:    
+        x += vel
+    if not(isJump):    
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < screenHeight - height - vel:        
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
+
+    win.fill((0,0,0))    
+    pygame.draw.rect(win, (255,0,0), (x,y,width,height))
+    pygame.display.update()  
+          
+
+pygame.quit()            
